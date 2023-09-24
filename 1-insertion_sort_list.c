9c7da4807917d6a3e48372bc4c296f9d5d6277dc
kseble <seble.temesgenk@gmail.com>
2023-09-24 11:41:45 -0700

    insert

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,10 +1,27 @@
 #include "sort.h"
 
 /**
+ * insertion_sort_list - sort algorithms with insertion
  *
+ * @list: A pointer to the head of a doubly-linked list of integers.
  *
+ * Description: Prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *iter, *insert, *tmp;
 
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	{
+		tmp = iter->next;
+		insert = iter->prev;
+
+		while (insert != NULL && iter->n < insert->n)
+		{
+			print_list((const listint_t *)*list);
+		}
+	}
 }
