d3bcf31cb61647b298c1b1118a496cd88e1f2124
kseble <seble.temesgenk@gmail.com>
2023-09-24 03:16:53 -0700

    add

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -23,27 +23,8 @@ void bubble_sort(int *array, size_t size)
 
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
+				print_array(array, size);
 			}
 		}
 	}
 }
-/**
- * print_array - Prints an array of integers
- *
- * @array: The array to be printed
- * @size: Number of elements in @array
- *
-void print_array(const int *array, size_t size)
-{
-	size_t i;
-
-	i = 0;
-	while (array && i < size)
-	{
-		if (i > 0)
-			printf(", ");
-		printf("%d", array[i]);
-		++i;
-	}
-	printf("\n");
-}*/
